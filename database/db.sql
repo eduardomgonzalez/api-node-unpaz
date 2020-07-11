62a9f04fc9afbde5d42eab5057b2e60a15658a99
eduardomgonzalez <eduardomgonzalez01@gmail.com>
2020-07-11 11:43:25 -0300

    modifico archivo db

--- database/db.sql
+++ database/db.sql
@@ -10,7 +10,19 @@ CREATE TABLE tutoriales
 
 /* Para insertar datos */
 INSERT INTO tutoriales (titulo, descripcion, publicado)
-    VALUES ('PHP', 'Todo sobre PHP', true),
-           ('PHP1', 'Todo sobre PHP1', true),
-           ('PHP2', 'Todo sobre PHP2', true),
-           ('PHP3', 'Todo sobre PHP3', true);
+    VALUES ('Angular 10', 'Todo sobre Angular 10', true),
+           ('JavaScript', 'La guia moderna de JS', false),
+           ('ReactJS', 'Basico de ReactJS', true),
+           ('ElectronJS', 'Utiliza JS para crear app de escritorio', true),
+		   ('TypeScript', 'JavaScript con super poderes', true),
+           ('Java', 'Java Avanzado', false),
+           ('FUndamentos', 'Fundamentos de la programacion', false),
+		   ('Redes', 'Crea modelos reales de redes', true),
+           ('Matarial UI', 'Aprende este poderoso framework', true),
+           ('Bootstrap 4', 'Utiliza todo el poder de Bootstrap', false),
+		   ('Arduino', 'Crea robots impresionantes', true),
+           ('Electronica', 'Electronica basica', true),
+           ('Electricidad', 'Aprende las bases de la electricidad', true),
+		   ('Haking Etico', 'Intrudoccion al hacking', false),
+           ('Pentesting', 'Pentesting con Kali Linux', false),
+           ('Kotlin', 'Crea modernas apps', true);
